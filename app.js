96ebc3e04f3865bd75a9b583ddb7c55526c32f0f
Gabriel Pasquale <pasqualeunq@gmail.com>
2019-06-19 17:16:47 -0300

    kickoff

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const logger = require('morgan');
+
+const bodyParser = require('body-parser');
+
+// This will be our application entry. We'll setup our server here.
+
+const http = require('http');
+
+const app = express();
+
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+
+app.get('*', (req, res) =>
+  res.status(200).send({
+    message: 'Welcome to the beginning of nothingness.'
+  })
+);
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+console.log(`listen in port: ${port}`);
+
+module.exports = app;
