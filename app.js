6b52bf7345b622c8760bc45f171c39f2d84ab0b2
Gabriel Pasquale <pasqualeunq@gmail.com>
2019-09-10 19:10:28 -0300

    cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const http = require('http');
 const logger = require('./app/logger');
 const errors = require('./app/middlewares/errors');
 const { init } = require('./app/routes');
+const cors = require('cors');
 
 const app = express();
 
@@ -11,12 +12,8 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors());
+app.options('*', cors());
 
 init(app);
 
