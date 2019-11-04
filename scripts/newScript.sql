0e81847d99f26b008a543aa0654d0639fe2d4d2d
Gabriel Pasquale <pasqualeunq@gmail.com>
2019-11-03 21:26:30 -0300

    newScript

---
+++ scripts/newScript.sql
@@ -0,0 +1,158 @@
+-- Subjects
+-- 1
+INSERT INTO subjects (id,code,university,career,year_plan,subject,course_mode,subject_weekly_hours,subject_total_hours,subject_core,credits,url,created_at,updated_at) 
+VALUES (DEFAULT,'1324','UNQ','TPI','2015','Orga','cuatrimestral','12','66','obligatorio','120','url','2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 2
+INSERT INTO subjects (id,code,university,career,year_plan,subject,course_mode,subject_weekly_hours,subject_total_hours,subject_core,credits,url,created_at,updated_at) 
+VALUES (DEFAULT,'234','UNQ','TPI','2015','Algoritmos','cuatrimestral','12','66','obligatorio','120','url','2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 3
+INSERT INTO subjects (id,code,university,career,year_plan,subject,course_mode,subject_weekly_hours,subject_total_hours,subject_core,credits,url,created_at,updated_at) 
+VALUES (DEFAULT,'343','UNLA','TPI','2015','Labo','cuatrimestral','12','66','obligatorio','120','url','2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 4
+INSERT INTO subjects (id,code,university,career,year_plan,subject,course_mode,subject_weekly_hours,subject_total_hours,subject_core,credits,url,created_at,updated_at) 
+VALUES (DEFAULT,'454','UNLA','Informatica','2015','poo 1','cuatrimestral','12','66','obligatorio','120','url','2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 5
+INSERT INTO subjects (id,code,university,career,year_plan,subject,course_mode,subject_weekly_hours,subject_total_hours,subject_core,credits,url,created_at,updated_at) 
+VALUES (DEFAULT,'35','UNLA','Informatica','2015','poo 2','cuatrimestral','12','66','obligatorio','120','url','2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 6
+INSERT INTO subjects (id,code,university,career,year_plan,subject,course_mode,subject_weekly_hours,subject_total_hours,subject_core,credits,url,created_at,updated_at) 
+VALUES (DEFAULT,'35','ITBA','Ingenieria en Informatica','2015','Labo','cuatrimestral','12','66','obligatorio','120','url','2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 7
+INSERT INTO subjects (id,code,university,career,year_plan,subject,course_mode,subject_weekly_hours,subject_total_hours,subject_core,credits,url,created_at,updated_at) 
+VALUES (DEFAULT,'343','UNLA','TPI','2011','Labo','cuatrimestral','12','66','obligatorio','120','url','2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 8
+INSERT INTO subjects (id,code,university,career,year_plan,subject,course_mode,subject_weekly_hours,subject_total_hours,subject_core,credits,url,created_at,updated_at) 
+VALUES (DEFAULT,'454','UNLA','Informatica','2011','poo 1','cuatrimestral','12','66','obligatorio','120','url','2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 9
+INSERT INTO subjects (id,code,university,career,year_plan,subject,course_mode,subject_weekly_hours,subject_total_hours,subject_core,credits,url,created_at,updated_at) 
+VALUES (DEFAULT,'35','UNLA','Informatica','2011','poo 2','cuatrimestral','12','66','obligatorio','120','url','2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 10
+INSERT INTO subjects (id,code,university,career,year_plan,subject,course_mode,subject_weekly_hours,subject_total_hours,subject_core,credits,url,created_at,updated_at) 
+VALUES (DEFAULT,'234','UNQ','TPI','2015','Base de datos','cuatrimestral','12','66','obligatorio','120','url','2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+
+-- Files
+-- 1
+INSERT INTO files (id,file_number,name,surname,mail,dni,year_note,status,created_at,updated_at) 
+VALUES (DEFAULT,'0358/101','Pepita','Golondrina','f.n.autalan@gmail.com','3453546','2017',0,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 2
+INSERT INTO files (id,file_number,name,surname,mail,dni,year_note,status,created_at,updated_at) 
+VALUES (DEFAULT,'0358/102','Pepita','Golondrina','f.n.autalan@gmail.com','3453546','2017',0,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 3
+INSERT INTO files (id,file_number,name,surname,mail,dni,year_note,status,created_at,updated_at) 
+VALUES (DEFAULT,'0358/103','Pepita','Golondrina','f.n.autalan@gmail.com','3453546','2017',0,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 4
+INSERT INTO files (id,file_number,name,surname,mail,dni,year_note,status,created_at,updated_at) 
+VALUES (DEFAULT,'0358/104','Pepita','Golondrina','f.n.autalan@gmail.com','3453546','2017',0,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 5
+INSERT INTO files (id,file_number,name,surname,mail,dni,year_note,status,created_at,updated_at) 
+VALUES (DEFAULT,'0358/105','Pepita','Golondrina','f.n.autalan@gmail.com','3453546','2017',0,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 6
+INSERT INTO files (id,file_number,name,surname,mail,dni,year_note,status,created_at,updated_at) 
+VALUES (DEFAULT,'0358/106','Pepita','Golondrina','f.n.autalan@gmail.com','3453546','2017',0,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 7
+INSERT INTO files (id,file_number,name,surname,mail,dni,year_note,status,created_at,updated_at) 
+VALUES (DEFAULT,'0358/107','Pepita','Golondrina','f.n.autalan@gmail.com','3453546','2017',0,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 8
+INSERT INTO files (id,file_number,name,surname,mail,dni,year_note,status,created_at,updated_at) 
+VALUES (DEFAULT,'0358/108','Pepita','Golondrina','f.n.autalan@gmail.com','3453546','2017',0,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 9
+INSERT INTO files (id,file_number,name,surname,mail,dni,year_note,status,created_at,updated_at) 
+VALUES (DEFAULT,'0358/109','Pepita','Golondrina','f.n.autalan@gmail.com','3453546','2017',0,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 10
+INSERT INTO files (id,file_number,name,surname,mail,dni,year_note,status,created_at,updated_at) 
+VALUES (DEFAULT,'0358/110','Pepita','Golondrina','f.n.autalan@gmail.com','3453546','2017',0,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+
+-- Requests
+-- 1 total match
+INSERT INTO requests (id,year_of_equivalence,signature,equivalence,observations,fk_fileid,fk_subjectid,created_at,updated_at) 
+VALUES (DEFAULT,'2017','Fidel','SIN EVALUAR','N/A',1,1,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 2 total match aprobada
+INSERT INTO requests (id,year_of_equivalence,signature,equivalence,observations,fk_fileid,fk_subjectid,created_at,updated_at) 
+VALUES (DEFAULT,'2017','Fidel','APROBADA','N/A',2,1,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 3 falta 1 para total match
+INSERT INTO requests (id,year_of_equivalence,signature,equivalence,observations,fk_fileid,fk_subjectid,created_at,updated_at) 
+VALUES (DEFAULT,'2017','Fidel','SIN EVALUAR','N/A',3,1,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 4 total match year
+INSERT INTO requests (id,year_of_equivalence,signature,equivalence,observations,fk_fileid,fk_subjectid,created_at,updated_at) 
+VALUES (DEFAULT,'2017','Fidel','SIN EVALUAR','N/A',4,2,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 5 total match aprobada year
+INSERT INTO requests (id,year_of_equivalence,signature,equivalence,observations,fk_fileid,fk_subjectid,created_at,updated_at) 
+VALUES (DEFAULT,'2017','Fidel','APROBADA','N/A',5,2,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 6 falta 1 para total match year
+INSERT INTO requests (id,year_of_equivalence,signature,equivalence,observations,fk_fileid,fk_subjectid,created_at,updated_at) 
+VALUES (DEFAULT,'2017','Fidel','SIN EVALUAR','N/A',6,2,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 7 match con distintas universidades
+INSERT INTO requests (id,year_of_equivalence,signature,equivalence,observations,fk_fileid,fk_subjectid,created_at,updated_at) 
+VALUES (DEFAULT,'2017','Fidel','APROBADA','N/A',7,1,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 8 negadas sin evaluar
+INSERT INTO requests (id,year_of_equivalence,signature,equivalence,observations,fk_fileid,fk_subjectid,created_at,updated_at) 
+VALUES (DEFAULT,'2017','Fidel','SIN EVALUAR','N/A',8,10,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 9 negadas
+INSERT INTO requests (id,year_of_equivalence,signature,equivalence,observations,fk_fileid,fk_subjectid,created_at,updated_at) 
+VALUES (DEFAULT,'2017','Fidel','NEGADA','N/A',9,10,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 10 negadas 2
+INSERT INTO requests (id,year_of_equivalence,signature,equivalence,observations,fk_fileid,fk_subjectid,created_at,updated_at) 
+VALUES (DEFAULT,'2017','Fidel','NEGADA','N/A',10,10,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+
+-- request_subjects
+-- request 1
+-- 1 
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,1,3,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 2
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,1,4,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 3
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,1,5,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- request 2
+-- 4
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,2,3,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 5
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,2,4,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 6
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,2,5,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- request 3
+-- 7
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,3,3,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 8
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,3,4,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- request 4
+-- 9
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,4,7,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 10
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,4,8,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 11
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,4,9,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- request 5
+-- 12
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,5,7,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 13
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,5,8,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 14
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,5,9,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- request 6
+-- 15
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,6,7,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 16
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,6,8,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- request 7
+-- 17
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,7,5,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 18
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,7,6,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 19
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,7,7,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- request 8
+-- 20
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,8,4,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 21
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,8,5,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- request 9
+-- 22
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,9,4,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 23
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,9,5,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- request 10
+-- 24
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,10,8,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+-- 25
+INSERT INTO request_subjects (id,request_id,subject_id,created_at,updated_at) VALUES (DEFAULT,10,9,'2019-10-11 19:25:37.078 +00:00','2019-10-11 19:25:37.078 +00:00');
+
